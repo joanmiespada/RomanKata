bfb3046d6fc94c5ac1bebbbd970afe3c82318b51
Joan Miquel Espada Sabat <joanmi@espada.cat>
2017-04-02 00:32:06 +0200

    entry point

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "RomanKata"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+
+mainClass := Some("RomanKata")
---
+++ src/main/scala-2.12/RomanKata.scala
@@ -0,0 +1,15 @@
+package RomanKata;
+/**
+  * Created by joanmi on 29/3/17.
+  */
+object RomanKata {
+
+  def main(args: Array[String]): Unit = {
+
+    println("test")
+
+  }
+
+
+
+}
